348ae5ffffc1084ec85ff504848f2eb402b4abfa
Logan Darklock <logandarklock@gmail.com>
2020-04-26 02:06:57 -0700

    Upgrade everything

--- src/main/kotlin/net/fabricmc/example/ExampleMod.kt
+++ src/main/kotlin/net/fabricmc/example/ExampleMod.kt
@@ -4,10 +4,9 @@ package net.fabricmc.example
 
 @Suppress("unused")
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
+	// This code runs as soon as Minecraft is in a mod-load-ready state.
+	// However, some things (like resources) may still be uninitialized.
+	// Proceed with mild caution.
 
-    println("Hello Fabric world!")
+	println("Hello Fabric world!")
 }
-
